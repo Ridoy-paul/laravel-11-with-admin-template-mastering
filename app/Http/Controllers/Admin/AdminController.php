f86b3de53c965c8a076222736dff23991c5fa12d
Ridoy-paul <cse.ridoypaul@gmail.com>
2024-05-04 17:37:35 +0600

    admin dashboard updated

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       
+    }
+
+    public function index()
+    {
+        return view('admin.pages.dashboard.home');
+    }
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect()->route('admin.index');
 });
+
+Route::group(['prefix'=>'admin', 'as'=>'admin.'], function(){
+
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/index', 'index')->name('index');
+    });
+
+});
+
